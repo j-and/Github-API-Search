74e8575ccc49f34c24b144a4b092db12a40cc2ef
j_and <you@example.com>
2016-09-21 10:17:38 +0300

    Fix build after merge

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { Home } from './home';
 import { About } from './about';
+import {NoContent} from "./no-content/no-content";
 
 
 export const ROUTES: Routes = [
